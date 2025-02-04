06544570e0449194d20493d78507878582c82859
sanketwaghmare4503 <sanket.waghmare@atomostech.com>
2025-02-04 17:02:11 +0530

    redux removed from index

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { Provider } from 'react-redux'
-import store from './utility/store.ts'
+
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
